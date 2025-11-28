9db9b3f042bfab0e5d10b8948b66726d0106796f
Fares Mohamed <119041871+Faress-MoHamed@users.noreply.github.com>
2025-11-28 20:34:03 +0200

    Add PrismaPg adapter and CA certificate for Postgres
    Introduces the @prisma/adapter-pg dependency and configures Prisma to use PrismaPg with a connection string and CA certificate for secure Postgres connections. Adds the CA certificate file and updates prisma.ts to use the new adapter.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,16 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaPostgresAdapter } from "@prisma/adapter-ppg";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const connectionString = `postgres://avnadmin:AVNS_-wSBsbg-x5KZud7HU9M@pg-320ef1ad-fareess-466d.k.aivencloud.com:22508/defaultdb?sslmode=verify-full&sslrootcert=certs/ca.pem`
+const adapter = new PrismaPg({ connectionString })
 
 export const prisma =
 	globalForPrisma.prisma ||
 	new PrismaClient({
 		log: ["error", "warn"],
-		adapter: new PrismaPostgresAdapter({
-			connectionString: process.env.DATABASE_URL!,
-		}),
+		adapter,
 	});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
